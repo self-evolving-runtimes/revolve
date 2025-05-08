c142824c9ea38f1e2a868b6f3be4a7a8fa78b9e3
drmehmetbilen <mehmetbilentr@gmail.com>
2025-05-08 14:35:31 +0300

    including cors, readme and .env variables

--- src/revolve/source_template/api.py
+++ src/revolve/source_template/api.py
@@ -6,6 +6,13 @@ from wsgiref.simple_server import make_server, WSGIServer
 import traceback
 import json
 import falcon
+import falcon_cors
+
+cors = falcon_cors.CORS(
+    allow_all_origins=True,
+    allow_all_headers=True,
+    allow_all_methods=True
+)
 
 ###IMPORTS###
 #from hellodb import HelloDBResource (just an example, not implemented)
@@ -29,7 +36,7 @@ def debug_error_serializer(req, resp, exception):
 LOGLEVEL = os.environ.get("LOGLEVEL", "DEBUG").upper()
 logging.basicConfig(level=LOGLEVEL)
 logger = logging.getLogger(__name__)
-app = falcon.App()
+app = falcon.App(middleware=[cors.middleware])
 
 app.set_error_serializer(debug_error_serializer)
 
