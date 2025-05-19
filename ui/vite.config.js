46aa53748b6a36f637e8ca3237889528b17cd010
drmehmetbilen <74433894+drmehmetbilen@users.noreply.github.com>
2025-05-19 19:18:28 +0300

    ui and python api configuration / handshake

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3008',
+        target: 'http://localhost:48001',
         changeOrigin: true,
         secure: false,
       },
