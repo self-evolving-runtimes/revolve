bdc549d382b3747537e73a030055c34d2a9ed494
drmehmetbilen <74433894+drmehmetbilen@users.noreply.github.com>
2025-05-19 21:12:14 +0300

    fixing db config pass

--- src/revolve/workflow_generator.py
+++ src/revolve/workflow_generator.py
@@ -1,14 +1,14 @@
 from queue import SimpleQueue
 from src.revolve.main import run_workflow
 
-def run_workflow_generator(task):
+def run_workflow_generator(task, db_config=None):
     q = SimpleQueue()
 
     def send(msg):
         q.put(msg)
 
     def runner():
-        run_workflow(task=task, send=send)
+        run_workflow(task=task, db_config=db_config, send=send)
         q.put(None)  # sentinel to stop
 
     import threading
