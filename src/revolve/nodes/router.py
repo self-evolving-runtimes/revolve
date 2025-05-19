41002c4661a9584d1e73162f5fb7753945903064
drmehmetbilen <74433894+drmehmetbilen@users.noreply.github.com>
2025-05-19 16:44:59 +0300

    cleaning code - reorganizing nodes
     reorganizing nodes,

---
+++ src/revolve/nodes/router.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+
+from src.revolve.data_types import State
+from src.revolve.functions import log, save_state
+from src.revolve.utils_git import init_or_attach_git_repo, create_branch_with_timestamp
+
+
+def router_node(state: State):
+    # ---------------------------------------
+    # ROUTE with LLM Decisions like an Agent, we can use the LLM to decide the next step
+    # messages = [
+    #     {
+    #         "role": "system",
+    #         "content": "Based on user request select the next path to take. The options are: generate_prompt_for_code_generation, do_stuff, do_other_stuff. If none of these options are relevant, return __end__."
+    #     },
+    #     {
+    #         "role": "user",
+    #         "content": state["messages"][-1].content
+    #     }
+    # ] 
+
+    # llm_router_response = llm_router.invoke(messages)
+    # return {
+    #     "next_node": llm_router_response.name
+    # }
+    # ---------------------------------------
+
+    # Route placeholder
+    next_node = state.get("next_node", None)
+    test_status = state.get("test_status", None)
+    resources = state.get("resources", None)
+    dbSchema = state.get("DBSchema", None)
+
+    send = state.get("send")
+
+    if not next_node:
+
+        init_or_attach_git_repo()
+        branch_name = create_branch_with_timestamp()
+        log("router_node", f"Branch created: {branch_name}",send)
+        log("router_node", "defaulting to generate_prompt_for_code_generation", send)
+        new_trace = {
+            "node_name": "router_node",
+            "node_type": "router",
+            "node_input": None,
+            "node_output": None,
+            "trace_timestamp": datetime.now(),
+            "description": "Routing to generate_prompt_for_code_generation"
+        }
+        return {
+            "next_node": "generate_prompt_for_code_generation",
+            "trace": [new_trace],
+        }
+    elif not test_status and resources and dbSchema:
+        save_state(state, "after_generation")
+        test_status = []
+        dbSchema = state.get("DBSchema")
+        for res, table_object in zip(resources,dbSchema.get("tables")):
+            new_test = {}
+            new_test["resource_file_name"] = res["resource_file_name"]
+            new_test["resource_code"] = res["resource_code"]
+            new_test["status"] = "in_progress"
+            new_test["messages"] = []
+            new_test["iteration_count"] = 0
+            new_test["table"] = table_object
+            test_status.append(new_test)
+                    
+        log("router_node", f"Routing to test_node", send)
+        new_trace = {
+            "node_name": "router_node",
+            "node_type": "router",
+            "node_input": None,
+            "node_output": None,
+            "trace_timestamp": datetime.now(),
+            "description": "Routing to test_node"
+        }
+        return {
+            "next_node": "test_node",
+            "test_status": test_status,
+            "trace": [new_trace],
+        }
+    elif next_node == "test_node":
+        next_node = "report_node"
+        log("router_node", f"Routing to report_node")
+        new_trace = {
+            "node_name": "router_node",
+            "node_type": "router",
+            "node_input": None,
+            "node_output": None,
+            "trace_timestamp": datetime.now(),
+            "description": "Routing to report_node"
+        }
+        return {
+            "next_node": next_node,
+            "trace": [new_trace],
+        }
+
+    else:
+        save_state(state, "after_test")
+        log("router_node", "routing to END", send)
+        return {
+            "next_node": "__end__",
+        }
---
+++ src/revolve/nodes/test.py
@@ -0,0 +1,206 @@
+
+from datetime import datetime
+from revolve.data_types import CodeHistoryMessage, GeneratedCode, State
+from revolve.functions import check_schema_if_has_foreign_key, log, read_python_code, read_python_code_template, run_pytest, save_python_code
+from revolve.prompts import get_test_generation_prompt, get_test_generation_prompt_ft, get_test_revising_prompt, get_test_revising_prompt_ft
+from revolve.llm import invoke_llm
+from revolve.utils import create_report_json
+from revolve.utils_git import commit_and_push_changes
+
+
+def test_node(state: State):
+    send = state.get("send")
+    MAX_TEST_ITERATIONS = 3
+    test_example = read_python_code_template("test_api.py")
+    utils = read_python_code_template("utils.py")
+    api_code = read_python_code("api.py")
+    for test_item in state["test_status"]:
+        is_foreign_key_exist = check_schema_if_has_foreign_key(test_item["table"]["columns"])
+        if is_foreign_key_exist:
+            log("test_node", f"Skipping test generation for {test_item['resource_file_name']} as it has foreign key", send)
+            test_item["status"] = "skipped"
+            continue
+        resouce_file = read_python_code(test_item["resource_file_name"])
+        test_file_name = "test_"+test_item["resource_file_name"]
+        log("test_node", f"Creating and testing for {test_file_name}", send)
+        table_name = test_item["table"]["table_name"]
+        schema = str(test_item["table"]["columns"])
+        
+        messages = get_test_generation_prompt(
+            test_example=test_example,
+            api_code=api_code,
+            table_name=table_name,
+            schema=schema,
+            utils=utils,
+            resouce_file=resouce_file,
+            resource_file_name = test_item["resource_file_name"]
+        )
+        messages_ft = get_test_generation_prompt_ft(
+            test_example=test_example,
+            api_code=api_code,
+            table_name=table_name,
+            schema=schema,
+            utils=utils,
+            resouce_file=resouce_file,
+            resource_file_name = test_item["resource_file_name"]
+        )
+
+        structured_test_response = invoke_llm(messages, max_attempts=3, validation_class=GeneratedCode, method="function_calling")
+        full_test_code = structured_test_response["full_test_code"]
+
+        assistan_response_ft = f"""
+### ASSISTANT ###
+#### Test Code ({test_file_name}) ####
+{full_test_code}
+"""
+
+        messages_ft.append(
+            {
+                "role": "assistant",
+                "content": assistan_response_ft
+            }
+        )
+
+
+        test_item["test_code"] = structured_test_response["full_test_code"]
+        test_item["test_file_name"] = test_file_name
+        save_python_code(
+            structured_test_response["full_test_code"],
+            test_file_name
+        )
+        commit_and_push_changes(
+            message=f"Test code generated for {test_item['resource_file_name']}"
+        )
+        pytest_response  = run_pytest(test_file_name)
+        test_item["status"] = pytest_response["status"]
+
+        test_item["code_history"] = test_item.get("code_history", [])
+        code_history_item = {
+            "history_type": "creation",
+            "code": {
+                "new_code": "N/A",
+                "what_was_the_problem": "N/A",
+                "what_is_fixed": "N/A",
+                "code_type": "N/A"
+            },
+            "test_report_before_revising": None,
+            "test_report_after_revising": pytest_response,
+            "iteration_index": 0
+        }
+        test_item["code_history"].append(code_history_item)
+        test_item["test_generation_input_prompt"] = messages_ft
+
+        for i in range(MAX_TEST_ITERATIONS):
+
+            #get the previous code history and add pytest_response to the test_report_after_revising
+            if pytest_response["status"]!= "success":
+                test_item["status"] = "failed"
+        
+
+                individual_prompt = test_item["table"]["individual_prompt"]
+                source_code = read_python_code(test_item["resource_file_name"])
+                test_code = read_python_code(test_file_name)
+                example_resource_code = read_python_code_template("service.py")
+                
+                new_messages = get_test_revising_prompt(
+                    individual_prompt=individual_prompt,
+                    source_code=source_code,
+                    example_resource_code=example_resource_code,
+                    test_code=test_code,
+                    api_code=api_code,
+                    table_name=table_name,
+                    schema=schema,
+                    utils=utils,
+                    pytest_response=pytest_response,
+                    resource_file_name = test_item["resource_file_name"]
+                )
+
+                new_messages_ft  = get_test_revising_prompt_ft(
+                    individual_prompt=individual_prompt,
+                    source_code=source_code,
+                    example_resource_code=example_resource_code,
+                    test_code=test_code,
+                    api_code=api_code,
+                    table_name=table_name,
+                    schema=schema,
+                    utils=utils,
+                    pytest_response=pytest_response,
+                    resource_file_name = test_item["resource_file_name"]
+                )
+
+
+                
+                new_test_code_response = invoke_llm(new_messages, max_attempts=3, validation_class=CodeHistoryMessage, method="function_calling", manual_validation=True)
+                test_item["iteration_count"] += 1
+
+                if new_test_code_response.code_type == "resource":
+                    file_name_to_revise = test_item["resource_file_name"]
+                elif new_test_code_response.code_type == "test":
+                    file_name_to_revise = test_file_name
+                else:
+                    file_name_to_revise = "api.py"
+
+                save_python_code(
+                    new_test_code_response.new_code,
+                    file_name_to_revise
+                )
+                commit_description = f"""What was the problem: {new_test_code_response.what_was_the_problem}
+What is fixed: {new_test_code_response.what_is_fixed}
+"""
+                new_messages_ft.append(
+                    {
+                        "role": "assistant",
+                        "content": f"""
+### ASSISTANT ###
+#### New Code ({file_name_to_revise}) ####
+{new_test_code_response.new_code}
+"""
+                    }
+                )
+
+                code_history_item = {
+                    "history_type": "revision",
+                    "code": {
+                        "new_code": new_test_code_response.new_code,
+                        "what_was_the_problem": new_test_code_response.what_was_the_problem,
+                        "what_is_fixed": new_test_code_response.what_is_fixed,
+                        "code_type": new_test_code_response.code_type
+                    },
+
+                    "test_revising_input_prompt" : new_messages_ft,
+                    "test_report_before_revising": pytest_response,
+                    "test_report_after_revising": None,
+                    "iteration_index": test_item["iteration_count"]
+                }
+
+                pytest_response  = run_pytest(test_file_name)
+                test_item["status"] = pytest_response["status"]
+
+
+                code_history_item["test_report_after_revising"] = pytest_response
+
+                test_item["code_history"].append(code_history_item)
+                create_report_json(state)
+                commit_and_push_changes(
+                    message=f"Code revised for {file_name_to_revise}",
+                    description=commit_description
+                )
+
+                if code_history_item["test_report_after_revising"]["summary"]==code_history_item["test_report_before_revising"]["summary"]:
+                    log("test_node", f"Test success is not changing, stopping the iteration: {test_item['iteration_count']}", send)
+                    break
+                
+                        
+            else:
+                break
+
+        
+    new_trace = {
+        "node_name": "test_node",
+        "node_type": "test",
+        "node_input": state["test_status"],
+        "node_output": state["test_status"],
+        "trace_timestamp": datetime.now(),
+        "description": "Test cases generated and executed."
+    }
+    return {"test_status": state["test_status"], "trace": [new_trace]}
