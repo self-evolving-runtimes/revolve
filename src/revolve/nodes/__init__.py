70b5e14d228b78fae4cf301c42a5069e92d82473
drmehmetbilen <74433894+drmehmetbilen@users.noreply.github.com>
2025-05-20 19:35:36 +0300

    adjusting some paths

--- src/revolve/nodes/__init__.py
+++ src/revolve/nodes/__init__.py
@@ -1,9 +1,9 @@
-from .router import router_node
+from revolve.nodes.router import router_node
 from revolve.nodes.generate_prompt import generate_prompt_for_code_generation
 from revolve.nodes.process_table import process_table
 from revolve.nodes.generate_api import generate_api
 from revolve.nodes.report import report_node
-from  src.revolve.nodes.test import test_node
+from revolve.nodes.test import test_node
 
 __all__ = [
     "router_node",
