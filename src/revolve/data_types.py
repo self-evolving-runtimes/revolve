98f5eaef6b806c743c49ae3f84e3433febc572ab
drmehmetbilen <mehmetbilentr@gmail.com>
2025-05-26 15:33:06 +0300

    support for enum and foreignkeys

--- src/revolve/data_types.py
+++ src/revolve/data_types.py
@@ -18,8 +18,9 @@ class Trace(TypedDict):
 
 class ForeignKey(TypedDict):
     """ Captures details about a foreign key relation to be used for API generation """
-    foreign_table: str
+    links_to_table: str
     foreign_column: str
+    rel_type:str
 
 class Column(TypedDict):
     """ Captures details about a specific column in a table for which API is being generated """
@@ -29,13 +30,20 @@ class Column(TypedDict):
     foreign_key: ForeignKey
     is_unique: bool
     is_nullable: bool
-    is_uid: str
+    is_uid: bool
+    enum_values: list[str]
+
+class RelatedTable(TypedDict):
+    """ Captures details about a related table to be used for API generation """
+    table_name: str
+    related_column: Column
 
 class Table(TypedDict):
     """ Captures details about an individual table for which API is being generated """
     individual_prompt: str
     table_name: str
     columns: list[Column]
+    related_tables: list[RelatedTable]
 
 class DBSchema(TypedDict):
     """Captures a list of tables for which APIs will get generated"""
