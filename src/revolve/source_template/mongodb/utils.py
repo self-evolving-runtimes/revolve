86f594b7abb9f41b06b4b73a2ec629bdbe3ce2ac
drmehmetbilen <mehmetbilentr@gmail.com>
2025-05-29 14:56:15 +0300

    updating utils template for mongodb
    It was getting this error:
    {'ok': 0.0, 'errmsg': 'Authentication failed.', 'code': 18, 'codeName': 'AuthenticationFailed'}

--- src/revolve/source_template/mongodb/utils.py
+++ src/revolve/source_template/mongodb/utils.py
@@ -4,13 +4,30 @@ from pymongo import MongoClient
 
 def get_db_connection(test_mode):
     try:
-        _db_name = os.getenv("DB_NAME") if not test_mode else os.getenv("DB_NAME_TEST")
         username = os.getenv("DB_USER")
         password = os.getenv("DB_PASSWORD")
         hostname = os.getenv("DB_HOST")
         port = os.getenv("DB_PORT", 27017)
 
-        uri = f"mongodb://{username}:{password}@{hostname}:{port}/{_db_name}"#  ?authSource=admin - more settings to consider
-        return MongoClient(uri)
+        client = MongoClient(
+            host=hostname,
+            port=int(port),
+            username=username,
+            password=password
+        )
+
+        return client
     except Exception as e:
         raise Exception(f"Database connection error: {e}")
+
+def get_db(client, test_mode):
+    """
+    Get a MongoDB database connection.
+    client: MongoClient instance
+    test_mode: boolean indicating if the connection is for testing
+    """
+    try:
+        _db_name = os.getenv("DB_NAME") if not test_mode else os.getenv("DB_NAME_TEST")
+        return client.get_database(_db_name)
+    except Exception as e:
+        raise Exception(f"Error getting database: {e}")
