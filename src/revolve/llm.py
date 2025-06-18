7d7411bc7737d69edf144e3d7178213603ee261f
drmehmetbilen <mehmetbilentr@gmail.com>
2025-06-18 14:54:03 +0300

    adding litellm to uv sync

--- src/revolve/llm.py
+++ src/revolve/llm.py
@@ -5,7 +5,19 @@ from revolve.data_types import *
 from langchain_openai import ChatOpenAI
 
 def invoke_llm(messages, max_attempts=3, validation_class=None, method="function_calling", logger=None, manual_validation=False):
-    llm  = ChatOpenAI(model=os.getenv("OPENAI_MODEL", "gpt-4.1"), temperature=0.2, max_tokens=16000)
+    llm_provider = os.environ.get("LLM_PROVIDER", "openai").lower()
+    if llm_provider == "openai":
+        return _invoke_openai_llm(messages, max_attempts, validation_class, method, logger, manual_validation)
+    elif llm_provider == "opensource":
+        return _invoke_opensource_llm(messages, max_attempts, validation_class, method, logger, manual_validation)
+    else:
+        raise ValueError(f"Unsupported LLM provider: {llm_provider}")
+
+def _invoke_opensource_llm(messages, max_attempts=3, validation_class=None, method="function_calling", logger=None, manual_validation=False):
+    pass
+
+def _invoke_openai_llm(messages, max_attempts=3, validation_class=None, method="function_calling", logger=None, manual_validation=False):
+    llm  = ChatOpenAI(model=os.getenv("MODEL_NAME", "gpt-4.1"), temperature=0.2, max_tokens=16000)
    
     if validation_class:
         llm = llm.with_structured_output(validation_class, method=method)
@@ -22,34 +34,3 @@ def invoke_llm(messages, max_attempts=3, validation_class=None, method="function
             if logger:
                 logger("Regenerating on ValidationError.")
     return None
-
-
-### GOOGLE - GEMINI
-### -------------------------- 
-# llm  = ChatOpenAI(model="gemini-2.5-pro-preview-05-06", temperature=0.2, max_tokens=16000, api_key=os.getenv("GEMINI_KEY"), base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
-# parse_method = "function_calling"
-# parallel = True
-### --------------------------
-
-
-### LLM - RUNPOD
-### -------------------------- ###
-# llm  = ChatOpenAI(model="Qwen/Qwen3-30B-A3B", temperature=0.1, max_tokens=16000, base_url="http://localhost:8000/v1/", api_key="no_needed")
-# parse_method = "function_calling"
-# parallel = False
-### -------------------------- ###
-
-
-### LLM - Local Ollama
-# --------------------------
-# llm  = ChatOpenAI(model="qwen3:30b-a3b", temperature=0.1, max_tokens=7000, base_url="http://localhost:11434/v1/", api_key="ollama")
-# parallel = False
-# --------------------------
-
-### Previous LLMs
-# llm_router = llm.with_structured_output(NextNode, method=parse_method)
-# llm_table_extractor = llm.with_structured_output(DBSchema, method=parse_method)
-# llm_resource_generator = llm.with_structured_output(Resource, method=parse_method)
-# llm_test_generator = llm.with_structured_output(GeneratedCode, method=parse_method)
-# llm_test_and_code_reviser = llm.with_structured_output(CodeHistoryMessage, method=parse_method)
-# llm_readme_generator = llm.with_structured_output(Readme, method=parse_method)
